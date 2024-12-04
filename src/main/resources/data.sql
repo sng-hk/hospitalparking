c842d581f44350ba5136815ef90d3b2729dfce6f
sng-hk <csh091456743@gmail.com>
2024-12-04 13:08:11 +0900

    amin 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,3 +14,7 @@ INSERT INTO parking_space (id, location) VALUES (12, 'C2');
 INSERT INTO parking_space (id, location) VALUES (13, 'C3');
 INSERT INTO parking_space (id, location) VALUES (14, 'C4');
 INSERT INTO parking_space (id, location) VALUES (15, 'C5');
+
+INSERT INTO user (rrn, phone_number, name, id, login_id, password, car_number, email) VALUES ('1234','1234','admin', 2, '1234', '$2a$10$Gao07nxHB9WVLclsXLU7.e9gnzm7gCOuiWNxYWG4UFqWg5Xj3U.6.', '1234', '123@gmail.com');
+INSERT INTO user_roles (user_id, roles) VALUES
+    (2, 'ROLE_ADMIN');
